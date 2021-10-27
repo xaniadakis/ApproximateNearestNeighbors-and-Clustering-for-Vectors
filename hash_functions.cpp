10871cdd43089fe4b9baaa705b8e618bc5cb0d1c
xaniadakis <echaniadakis@gmail.com>
2021-10-27 12:36:01 +0300

    commit

--- hash_functions.cpp
+++
@@ -1,8 +0,0 @@
-#include "hash_functions.hpp"
-#include <numeric>
-#include <cmath>
-
-int hash_L2(unsigned int i,vector<float> p,vector<float> *v,float *t,int w) {
-	float innerProduct = inner_product(p.begin(), p.end(), v[i].begin(), 0);
-	return floor((innerProduct+t[i])/w);
-}
--- hash_functions.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-using namespace std;
-#include <vector>
-
-int hash_L2(unsigned int i,vector<float> p,vector<float> *v,float *t,int w);
